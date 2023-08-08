7ac87f4774ad9228465abc1627a705d3a96d259a
Dexter Kong <78414602+dexico-gme@users.noreply.github.com>
2023-08-08 23:56:54 +0100

    modified:   book.js 	modified:   main.html 	modified:   style.css added add book modal

--- book.js
+++ book.js
@@ -61,7 +61,7 @@ function displayBook() {
   }
 }
 
-// document.getElementById("addABook").addEventListener("click", hi);
+document.getElementById("addABook").addEventListener("click", hi);
 
 const form = document.getElementById("submitBook");
 
@@ -88,3 +88,29 @@ function hi() {
 function openTheForm() {
   document.getElementById("popupForm").style.display = "block";
 }
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("add-book-btn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
